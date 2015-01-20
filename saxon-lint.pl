b0c8cab494c140d55f6e82b35887a66473b7ca65
sputnick <gilles.quenot@gmail.com>
2015-01-20 22:33:48 +0100

    Fixing --xquery errors due to quoting ²

--- saxon-lint.pl
+++ saxon-lint.pl
@@ -108,7 +108,7 @@ foreach my $input (@ARGV) {
     else {
         my $xml = qx(
         $verbose
-            java -cp "$classpath" "$mainclass" \Q-s:$input\E \Q$q:$query\E -quit:on !item-separator=\$'$oDel' !encoding=utf-8 !indent=$indent
+            java -cp "$classpath" "$mainclass" \Q-s:$input\E '$q:$query' -quit:on !item-separator=\$'$oDel' !encoding=utf-8 !indent=$indent
         );
         $res = $?;
 
