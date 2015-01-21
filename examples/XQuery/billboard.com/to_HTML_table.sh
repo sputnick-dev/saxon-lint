75387189de2e0138d6b4da08733273f57f62d38d
sputnick <gilles.quenot@gmail.com>
2015-01-21 15:43:02 +0100

    Added use of tee in an example script

--- examples/XQuery/billboard.com/to_HTML_table.sh
+++ examples/XQuery/billboard.com/to_HTML_table.sh
@@ -2,4 +2,4 @@
 
 # check http://htmlpreview.github.io/?https://raw.githubusercontent.com/sputnick-dev/saxon-lint/master/examples/XQuery/billboard.com/to_HTML_table.html
 
-./saxon-lint.pl --indent --html --xquery examples/XQuery/billboard.com/to_HTML_table.xquery http://www.billboard.com/charts/billboard-200 > examples/XQuery/billboard.com/to_HTML_table.html
+./saxon-lint.pl --indent --html --xquery examples/XQuery/billboard.com/to_HTML_table.xquery http://www.billboard.com/charts/billboard-200 | tee examples/XQuery/billboard.com/to_HTML_table.html
