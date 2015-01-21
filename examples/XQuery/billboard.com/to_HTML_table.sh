0b0460c529e793d1a6fffc21ff38673e84f6fbf1
sputnick <gilles.quenot@gmail.com>
2015-01-21 07:43:17 +0100

    Examples updated

--- examples/XQuery/billboard.com/to_HTML_table.sh
+++ examples/XQuery/billboard.com/to_HTML_table.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./saxon-lint.pl --indent --html --xquery examples/XQuery/billboard.com/to_HTML_table.xquery http://www.billboard.com/charts/billboard-200
+# check http://htmlpreview.github.io/?https://raw.githubusercontent.com/sputnick-dev/saxon-lint/master/examples/XQuery/billboard.com/to_HTML_table.html
+
+./saxon-lint.pl --indent --html --xquery examples/XQuery/billboard.com/to_HTML_table.xquery http://www.billboard.com/charts/billboard-200 > examples/XQuery/billboard.com/to_HTML_table.html
