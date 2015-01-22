7ab9d12f2e64623dfde4389eaaca93c865d1b9ae
sputnick <gilles.quenot@gmail.com>
2015-01-22 22:28:35 +0100

    Better descriptions and new test

--- t/001.pl
+++ t/001.pl
@@ -1,25 +1,22 @@
 #!/usr/bin/perl
 
-# to run the tests, you need an *Unix like shell and :
-# $ prove t/001.pl
+# to run the tests, you need an *Unix like shell and '$ prove t/001.pl'
 
 use utf8;
 use strict; use warnings;
 
-use Unicode::Normalize;
-use charnames ':full';
-
-use Test::More tests => 11;
+use Test::More tests => 12;
 
 like(qx(./examples/XPath/get_free_links.sh), qr!http://!, 'match http links');
-like(qx(./examples/XPath/001.sh), qr!1,2,3!, 'match');
-like(qx(./examples/XPath/002.sh), qr!1\n2\n3!s, 'match');
-like(qx(./examples/XQuery/billboard.com/to_HTML_table.sh), qr!Check file!, 'match');
-like(qx(cat examples/XQuery/billboard.com/to_HTML_table.html), qr!\R\s+<td>Hoodie Allen Featuring!, 'match');
+like(qx(./examples/XPath/001.sh), qr!1,2,3!, 'simple xpath concatenation');
+like(qx(./examples/XPath/002.sh), qr!1\n2\n3!s, 'simple xpath query on multi-lines');
+like(qx(./examples/XQuery/billboard.com/to_HTML_table.sh), qr!Check file!, 'xquery to create a table');
+like(qx(cat examples/XQuery/billboard.com/to_HTML_table.html), qr!\R\s+<td>Hoodie Allen Featuring!, 'test a node in generated HTML');
 my $x = qx(./examples/XQuery/billboard.com/simple_text_parsing.sh);
-like($x, qr!\[100\] All About It!, 'match');
-unlike($x, qr/xml\s+version/, 'not match');
-like(qx(examples/XQuery/FLWOR/books.sh), qr!\s+<title>Hui</title>!, 'match');
-like(qx(examples/XSLT/ex_01.sh), qr!\R\s+<target>num1,num2,num3</target>!, 'match');
-like(qx(./examples/XQuery/simple/variable_interpolation.sh), qr/Hello\s+World!/, 'match');
-like(qx(examples/XQuery/simple/doc_use.sh), qr!\R<title>Zetaz</title>!, 'match');
+like($x, qr!\[100\] All About It!, 'simple_text_parsing found [100]');
+unlike($x, qr/xml\s+version/, 'not matching PI');
+like(qx(examples/XQuery/FLWOR/books.sh), qr!\s+<title>Hui</title>!, 'FLWOR books query match a node');
+like(qx(examples/XSLT/ex_01.sh), qr!\R\s+<target>num1,num2,num3</target>!, 'xslt concatenation');
+like(qx(./examples/XQuery/simple/variable_interpolation.sh), qr/Hello\s+World!/, 'xquery variable interpolation');
+like(qx(examples/XQuery/simple/doc_use.sh), qr!\R<title>Zetaz</title>!, 'using fn:doc()');
+like(qx(examples/XQuery/simple/inline-XQyery.sh), qr!\R5!, 'inline xquery');
