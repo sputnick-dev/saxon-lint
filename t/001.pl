4bd594539cf38e6e5338d6f650d51bb14af884e6
sputnick <gilles.quenot@gmail.com>
2015-01-22 17:25:41 +0100

    Updated Unit Tests

--- t/001.pl
+++ t/001.pl
@@ -9,15 +9,16 @@ use strict; use warnings;
 use Unicode::Normalize;
 use charnames ':full';
 
-use Test::More tests => 9;
+use Test::More tests => 10;
 
 like(qx(./examples/XPath/get_free_links.sh), qr!http://!, 'match http links');
-like(qx(./examples/XPath/001.sh), qr!1,2,3!, 'match 1,2,3');
-like(qx(./examples/XPath/002.sh), qr!1\n2\n3!s, 'match 1,2,3');
-like(qx(./examples/XQuery/billboard.com/to_HTML_table.sh), qr!Check file!);
-like(qx(cat examples/XQuery/billboard.com/to_HTML_table.html), qr!\s+<td>Hoodie Allen Featuring!);
+like(qx(./examples/XPath/001.sh), qr!1,2,3!, 'match');
+like(qx(./examples/XPath/002.sh), qr!1\n2\n3!s, 'match');
+like(qx(./examples/XQuery/billboard.com/to_HTML_table.sh), qr!Check file!, 'match');
+like(qx(cat examples/XQuery/billboard.com/to_HTML_table.html), qr!\s+<td>Hoodie Allen Featuring!, 'match');
 my $x = qx(./examples/XQuery/billboard.com/simple_text_parsing.sh);
-like($x, qr!\[100\] All About It!);
-unlike($x, qr/xml\s+version/);
-like(qx(examples/XQuery/FLWOR/books.sh), qr!\s+<title>Hui</title>!);
-like(qx(examples/XSLT/ex_01.sh), qr!\s+<target>num1,num2,num3</target>!);
+like($x, qr!\[100\] All About It!, 'match');
+unlike($x, qr/xml\s+version/, 'not match');
+like(qx(examples/XQuery/FLWOR/books.sh), qr!\s+<title>Hui</title>!, 'match');
+like(qx(examples/XSLT/ex_01.sh), qr!\s+<target>num1,num2,num3</target>!, 'match');
+like(qx(./examples/XQuery/simple/variable_interpolation.sh), qr/Hello\s+World!/, 'match');
