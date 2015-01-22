75df3fcbd0044cdcd269a0c75547b852352dbb1a
sputnick <gilles.quenot@gmail.com>
2015-01-22 20:00:30 +0100

    Added tee command

--- examples/XQuery/billboard.com/to_HTML_table.sh
+++ examples/XQuery/billboard.com/to_HTML_table.sh
@@ -2,8 +2,8 @@
 
 # Check http://tinyurl.com/p5853wo
 
-if ./saxon-lint.pl --indent --html --xquery examples/XQuery/billboard.com/to_HTML_table.xquery examples/XQuery/billboard.com/input.html > examples/XQuery/billboard.com/to_HTML_table.html; then
-    echo "Check file://$PWD/examples/XQuery/billboard.com/to_HTML_table.html"
+if ./saxon-lint.pl --indent --html --xquery examples/XQuery/billboard.com/to_HTML_table.xquery examples/XQuery/billboard.com/input.html | tee examples/XQuery/billboard.com/to_HTML_table.html; then
+    printf "\n\nCheck file://$PWD/examples/XQuery/billboard.com/to_HTML_table.html\n"
 else
     echo >&2 "ERROR"
     exit 1
